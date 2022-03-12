9aa87f900fe624720e3058ae9d14dcdc9d0aaccc
Kdoggg666 <kenanwright@gmail.com>
2022-03-12 15:53:45 +0000

    Add weather Api call

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
+import requests
 from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+                   Flask, flash, render_template,
+                   redirect, request, session, url_for)
 from geopy.geocoders import Nominatim
 import geocoder
 
@@ -14,6 +15,18 @@ app = Flask(__name__)
 
 app.config["WEATHER_API_KEY"] = os.environ.get("WEATHER_API_KEY")
 app.config["MAPS_API_KEY"] = os.environ.get("MAPS_API_KEY")
+weather_api_key = os.environ.get("WEATHER_API_KEY")
+
+
+def get_weather_results(city, api_key):
+    """
+    Api call to openweather
+    """
+    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+    r = requests.get(api_url)
+    print(r.json())
+    return r.json()
+
 
 
 @app.route("/")
@@ -22,26 +35,29 @@ def recipes():
     """
     App route to show weather application
     """
+    
     g = geocoder.ip('me')
-    # print(g.latlng)
     cur_lat = g.latlng[0]
     cur_lng = g.latlng[1]
-    # print(g.latlng[1])
     geo_loc = Nominatim(user_agent="GetLoc")
-    locname = geo_loc.reverse(f'{cur_lat}, {cur_lng}', language='en', addressdetails=True)
+    locname = geo_loc.reverse(f'{cur_lat}, {cur_lng}',
+                              language='en',
+                              addressdetails=True)
     print(locname)
     print(f'{cur_lat}, {cur_lng}')
     my_location = locname.address
     loc_dict = g.raw
-    print(loc_dict['city'])
     city = loc_dict['city']
     country = loc_dict['country']
     region = loc_dict['region']
-    return render_template('weather.html', 
-                           my_location=my_location, 
+    weather_data = get_weather_results('gothenburg', weather_api_key)
+    temp = int(weather_data['main']['temp'])
+    return render_template('weather.html',
+                           my_location=my_location,
                            locname=locname,
                            city=city,
                            country=country,
+                           temp=temp,
                            region=region)
 
 
