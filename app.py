a4f0ffeb350549b9cde2cf3e624958285d960a64
Kdoggg666 <kenanwright@gmail.com>
2022-03-12 15:00:24 +0000

    Added geocoding to get location

--- app.py
+++ app.py
@@ -28,10 +28,21 @@ def recipes():
     cur_lng = g.latlng[1]
     # print(g.latlng[1])
     geo_loc = Nominatim(user_agent="GetLoc")
-    locname = geo_loc.reverse(f'{cur_lat}, {cur_lng}')
+    locname = geo_loc.reverse(f'{cur_lat}, {cur_lng}', language='en', addressdetails=True)
     print(locname)
     print(f'{cur_lat}, {cur_lng}')
-    return render_template('weather.html')
+    my_location = locname.address
+    loc_dict = g.raw
+    print(loc_dict['city'])
+    city = loc_dict['city']
+    country = loc_dict['country']
+    region = loc_dict['region']
+    return render_template('weather.html', 
+                           my_location=my_location, 
+                           locname=locname,
+                           city=city,
+                           country=country,
+                           region=region)
 
 
 if __name__ == "__main__":
